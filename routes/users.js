000261a914dee3a6a389a350ce9e52babe47dc6b
Fabian Lizarme <fabianslu2@gmail.com>
2015-07-14 17:24:25 -0600

    Add users function added

--- routes/users.js
+++ routes/users.js
@@ -17,5 +17,39 @@ router.get('/user-list-table', function(req, res) {
     });
 });
 
+/* GET New User page. */
+router.get('/new-user', function(req, res) {
+    res.render('new-user', { title: 'Add New User' });
+});
+
+router.post('/add-user', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+    console.log(req.body);
+    // Get our form values. These rely on the "name" attributes
+    var name = req.body.name;
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // Set our collection
+    var collection = db.get('users');
+
+    // Submit to the DB
+    collection.insert({
+        "name" : name,
+        "username" : username,
+        "password" : password
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("user-list-table");
+        }
+    });
+});
 
 module.exports = router;
